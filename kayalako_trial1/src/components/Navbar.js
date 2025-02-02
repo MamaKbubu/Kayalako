578ac99a2e97b195fdb948b778517c7ebd584f01
MamaKbubu <maboelatk@gmail.com>
2025-02-02 19:52:02 +0200

    1st edit

--- kayalako_trial1/src/components/Navbar.js
+++ kayalako_trial1/src/components/Navbar.js
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "@/styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
+      {/* Logo aligned to the left */}
+      <Link href="/">
+        <Image src="/logo.jpg" alt="Logo" width={100} height={100} />
+      </Link>
+
+      {/* Links aligned to the right */}
       <ul>
         <li>
           <Link href="/">Home</Link>
